082bc82d9848cf9366458b620fb2beea807fe80d
Tobias Wilken <tooangel@tooangel.de>
2017-06-23 23:37:02 +0200

    Improve planer handling

--- src/prototype_creep.js
+++ src/prototype_creep.js
@@ -165,7 +165,7 @@ Creep.prototype.stayInRoom = function() {
 
 Creep.prototype.buildRoad = function() {
   // TODO make dependent on the swamp to non-swamp relation? High swamp rooms could use the roads better ...
-  if (this.room.controller && (this.room.controller.level < 4 || this.room.memory.misplacedSpawn)) {
+  if (this.room.controller && (this.room.controller.level < 3 || this.room.memory.misplacedSpawn)) {
     return false;
   }
 
---
+++ src/role_quester.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+ * quester moves around and distributes quests
+ */
+
+roles.quester = {};
+roles.quester.settings = {
+  layoutString: 'M',
+  maxLayoutAmount: 1,
+};
+
+roles.quester.execute = function(creep) {
+
+};
